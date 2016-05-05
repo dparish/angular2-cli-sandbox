23f2e24870225479eb469cfcc54f096114b9f5a9
David Parish <dparish@gmail.com>
2016-05-05 10:39:48 -0500

    Adding a route

--- src/app/test-app.component.ts
+++ src/app/test-app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TasksComponent } from './+tasks';
+import { Routes, Router, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'test-app-app',
   templateUrl: 'test-app.component.html',
-  styleUrls: ['test-app.component.css']
+  styleUrls: ['test-app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
-export class TestAppAppComponent {
+@Routes([
+  {path: '/tasks', component: TasksComponent}
+])
+export class TestAppAppComponent implements OnInit{
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.navigate(['/tasks']);
+  }
+
   title = 'test-app works!';
 }
